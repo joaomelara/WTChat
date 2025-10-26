0da4b927961e599996eae435effb53485f616822
joaomelara <joaopedromelara@gmail.com>
2025-10-26 00:43:53 -0300

    Navigation done

--- app/src/main/java/com/example/wtchat/screens/ConversationHubScreen.kt
+++ app/src/main/java/com/example/wtchat/screens/ConversationHubScreen.kt
@@ -3,27 +3,18 @@ package com.example.wtchat.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import com.example.wtchat.Routes
 
 @Composable
-fun LoginScreen(navController: NavController){
-    Column (
+fun ConversationHubScreen(nameTest : String){
+    Column(
         Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
-    ){
-        Text(text = "LoginScreen")
-        Button(onClick = {
-            navController.navigate(Routes.ConversationHubScreen+"/john")
-        }) {
-            Text(text = "GOTO Conversations")
-
-        }
+    ) {
+        Text(text = "Conversation Screen and nameTest is $nameTest")
     }
 }
