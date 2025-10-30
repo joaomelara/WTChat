01c39670f39d0708e22415945332fb9a14cf18ca
Furover <special.beetle@protonmail.com>
2025-10-30 16:54:41 -0300

    fixed merge issues

--- app/src/main/java/com/example/wtchat/pages/HubPage.kt
+++ app/src/main/java/com/example/wtchat/pages/HubPage.kt
@@ -1,29 +1,77 @@
 package com.example.wtchat.pages
 
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
-import com.example.wtchat.Routes
+import com.example.wtchat.R
 import com.example.wtchat.viewmodels.AuthState
 import com.example.wtchat.viewmodels.AuthViewModel
+import com.example.wtchat.Routes
+import com.example.wtchat.models.ChatModel
+import com.example.wtchat.ui.theme.WTCBackground
+import com.example.wtchat.ui.theme.WTCBlue
+import com.example.wtchat.ui.theme.WTCGrey
+import com.example.wtchat.ui.theme.WTCOrange
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 
 @Composable
 fun HubPage(navController: NavController ,authViewModel: AuthViewModel){
+
+    var pesquisa = remember {
+        mutableStateOf("")
+    }
+
+    var conversas = remember {
+        mutableStateOf<List<ChatModel>>(emptyList())
+    }
+
+    var userNome = ""
+
     var context = LocalContext.current
 
     val authState = authViewModel.authState.observeAsState()
@@ -33,26 +81,111 @@ fun HubPage(navController: NavController ,authViewModel: AuthViewModel){
             is AuthState.Unauthenticated -> navController.navigate(Routes.LoginScreen){
                 popUpTo(navController.graph.findStartDestination().id) { inclusive = true }
             }
+            is AuthState.Authenticated -> {
+                Firebase.firestore.collection("chats").whereArrayContains("participantes",
+                    FirebaseAuth.getInstance().currentUser?.uid!!).get().addOnCompleteListener {
+                    if(it.isSuccessful) {
+                        val results = it.result.documents.mapNotNull { doc ->
+                            doc.toObject(ChatModel::class.java)
+                        }
+                        conversas.value = results
+                    }
+                }
+
+                Firebase.firestore.collection("users")
+                    .document(FirebaseAuth.getInstance().currentUser?.uid!!).get().addOnCompleteListener {
+                        if(it.isSuccessful) {
+                            userNome = it.result.getString("nome")!!
+                        }
+                    }
+
+            }
             else -> Unit
         }
     }
 
-    Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = WTCBackground
     ) {
-        Text(text = "Conversation Screen and nameTest is gaming")
-        Button(
-            onClick = {
-                authViewModel.signout()
-            },
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-            enabled = authState != AuthState.Loading
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Sair", color = MaterialTheme.colorScheme.onPrimary, modifier = Modifier.padding(8.dp))
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            // Title
+            Text(
+                text = "Conversas",
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = WTCBlue
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(20.dp),
+                value = pesquisa.value,
+                onValueChange = { novoValor ->
+                    pesquisa.value = novoValor
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                placeholder = {
+                    Text(text = "Pesquisar")
+                },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent, // Remove bottom border when focused
+                    unfocusedIndicatorColor = Color.Transparent, // Remove bottom border when unfocused
+                    unfocusedContainerColor = WTCGrey,
+                    focusedContainerColor = WTCGrey
+                ),
+
+                )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Text(text = "Filtros")
+
+            LazyColumn {
+                items(conversas.value) { item ->
+                    Spacer(modifier = Modifier.height(30.dp))
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .clickable(onClick = {
+                                navController.navigate(Routes.ConversationScreen+"/"+item.uid+"/"+item.titulo+"/"+userNome)
+                            }),
+                        verticalAlignment = Alignment.CenterVertically,
+
+                        ) {
+                        Box(
+                            modifier = Modifier.background(WTCBlue, RoundedCornerShape(200.dp))
+                                .size(65.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Icon(imageVector = Icons.Default.Person, contentDescription = "Chat Icon", modifier =  Modifier.size(40.dp), tint = WTCBackground)
+                        }
+
+                        Spacer(modifier = Modifier.size(20.dp))
+
+                        Text(
+                            style = MaterialTheme.typography.titleMedium,
+                            text = item.titulo
+                        )
+
+                    }
+                }
+            }
+
         }
     }
 
 }
+
